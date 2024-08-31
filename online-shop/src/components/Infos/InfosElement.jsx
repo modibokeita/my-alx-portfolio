47cab0efd53bbea7445d75d809bd4e7c358fa627
modibo keita <93909081keita@gmail.com>
2024-08-31 12:52:33 +0300

    infos elements

---
+++ online-shop/src/components/Infos/InfosElement.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const InfosContainer = styled.div`
+
+`
+export const InfosWrapper = styled.div`
+
+`
+export const InfosRow = styled.div`
+
+`
+export const Column1 = styled.div`
+
+`
+export const TextWrapper = styled.div`
+
+`
+export const TopLine = styled.div`
+
+`
+export const Heading = styled.div`
+
+`
+export const Subtitle = styled.div`
+
+`
+export const BtnWraper = styled.div`
+
+`
--- online-shop/src/components/Infos/index.jsx
+++ online-shop/src/components/Infos/index.jsx
@@ -1,12 +1,33 @@
-
-
-function Infos() {
+import { Button } from "react-scroll"
+import PropTypes from "prop-types"
+import {InfosContainer, InfosWrapper, InfosRow
+    , Column1, TextWrapper, TopLine, Heading, Subtitle, BtnWraper
+} from "./InfosElement"
+function Infos({id}) {
   return (
-    <div>
-
-    </div>
+    <>
+        <InfosContainer id={id}>
+            <InfosWrapper>
+                <InfosRow>
+                    <Column1>
+                        <TextWrapper>
+                            <TopLine>Top Line</TopLine>
+                            <Heading>Heading</Heading>
+                            <Subtitle>Subtitle</Subtitle>
+                            <BtnWraper>
+                                <Button to="home">Button</Button>
+                            </BtnWraper>
+                        </TextWrapper>
+                    </Column1>
+                </InfosRow>
+            </InfosWrapper>
+        </InfosContainer>
+    </>
   )
 }
 
+Infos.propTypes = {
+    id: PropTypes.id
+}
 export default Infos
 

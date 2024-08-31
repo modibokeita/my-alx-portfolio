3d62df3e33b9aee49003747ea82f6be2532864de
modibo keita <93909081keita@gmail.com>
2024-08-31 13:37:21 +0300

    infos elements CSS

--- online-shop/src/pages/index.jsx
+++ online-shop/src/pages/index.jsx
@@ -2,6 +2,7 @@ import SideBar from "../components/SideBar"
 import NavBar from "../components/NavBar"
 import { useState } from "react";
 import LandingPage from "../components/LandingPage";
+import Infos from "../components/Infos";
 
 function Home() {
     const [isOpen, setIsOpen] = useState(false);
@@ -14,6 +15,7 @@ function Home() {
         <SideBar isOpen={isOpen} toggle={toggle}/>
         <NavBar toggle={toggle}/>
         <LandingPage />
+        <Infos />
     </>
   )
 }

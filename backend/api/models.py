835433fc28753928b8e270e36b095b2c096de551
modibo keita <93909081keita@gmail.com>
2024-09-19 01:10:44 +0300

    Added Models

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,24 +2,40 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Category(models.Model):
+    """
+     Category Model
+
+    """
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
 class Product(models.Model):
+    """
+     Product Model
+
+    """
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='product_images/', blank=True, null=True) 
+    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
 class Order(models.Model):
+    """ Order model for customers
+
+    Args:
+        model (Order): to get the order of the customers
+
+    Returns:
+        object: return user, total order
+    """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=20)
@@ -29,6 +45,14 @@ class Order(models.Model):
         return f"Order {self.id}"
 
 class OrderItem(models.Model):
+    """ OrderItem model for customers
+
+    Args:
+        model (OrderItem): to get the order Itmes of the customer
+
+    Returns:
+        object: return user, total ordered Items
+    """
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
@@ -38,15 +62,39 @@ class OrderItem(models.Model):
         return f"{self.quantity} of {self.product.name}"
 
 class Cart(models.Model):
+    """ Cart model
+
+    Args:
+        model (Cart): to get the selected Itmes of the customer
+
+    Returns:
+        object: return user, total selected Items
+    """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
 class CartItem(models.Model):
+    """ Cart Item model
+
+    Args:
+        model (Cart Item): to get the selected Items of the User
+
+    Returns:
+        object: return user, total selected Products
+    """
     cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
 
 class Review(models.Model):
+    """ Review model
+
+    Args:
+        model (Review): to allow User comments
+
+    Returns:
+        object: return user review
+    """
     product = models.ForeignKey(Product, related_name='reviews', on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     rating = models.IntegerField()
